63e0d6f3c1439cac06d800723bf3a53ea359c8f0
Seunghwan Kim <ysp1307@naver.com>
2022-10-26 18:37:28 +0900

    ✨ feat : question component 생성 (#16)

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,9 +1,9 @@
 import { AsideNav } from "../../components/AsideNav";
 import { NavBar } from "../../components/NavBar";
+import { Question } from "../../components/Question";
 
 export const Main = () => {
   return (
-    //justify-content: center;
     <div className="flex h-screen w-screen">
       <NavBar />
       <div className="flex pt-[60px] w-screen justify-center">
@@ -48,66 +48,7 @@ export const Main = () => {
             </div>
 
             <ul className="pr-6 divide-y divide-line-gray">
-              <li className="flex flex-nowrap p-4 w-full border border-x-0 border-b-0 border-t-line-gray bg-main-yellow ">
-                <div className="flex flex-col  w-28 flex-shrink-0 items-end text-font-gray mr-5 gap-1">
-                  <div className="text-sm text-black">0 votes</div>
-                  <div className="text-sm">10 answers</div>
-                  <div className="text-sm">100 views</div>
-                </div>
-                <div className="flex flex-col justify-between w-full">
-                  <div className="text-lg text-blue-500">
-                    Script to copy all text from one file into template file
-                  </div>
-                  <div className="flex justify-end pr-5 items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6 bg-main-gray rounded-lg p-1"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                      />
-                    </svg>
-                    <div className="text-blue-500 mx-2">seunghwan</div>
-                    <div> asked 6 hours ago</div>
-                  </div>
-                </div>
-              </li>
-              <li className="flex flex-nowrap p-4 w-full border border-x-0 border-b-0 border-t-line-gray bg-main-yellow ">
-                <div className="flex flex-col  w-28 flex-shrink-0 items-end text-font-gray mr-5 gap-1">
-                  <div className="text-sm text-black">0 votes</div>
-                  <div className="text-sm">10 answers</div>
-                  <div className="text-sm">100 views</div>
-                </div>
-                <div className="flex flex-col justify-between w-full">
-                  <div className="text-lg text-blue-500">
-                    Script to copy all text from one file into template file
-                  </div>
-                  <div className="flex justify-end pr-5 items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6 bg-main-gray rounded-lg p-1"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                      />
-                    </svg>
-                    <div className="text-blue-500 mx-2">seunghwan</div>
-                    <div> asked 6 hours ago</div>
-                  </div>
-                </div>
-              </li>
+              <Question />
             </ul>
           </div>
           <aside className="bg-blue-200 w-96 p-6 lg:block hidden">aside</aside>
