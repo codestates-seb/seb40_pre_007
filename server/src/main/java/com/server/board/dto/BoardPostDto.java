e657a9b78fd124d8c884582eadde36643c10f818
JungSoyi <soi0125@naver.com>
2022-10-28 01:12:47 +0900

    :art:

--- server/src/main/java/com/server/board/dto/BoardPostDto.java
+++ server/src/main/java/com/server/board/dto/BoardPostDto.java
@@ -1,5 +1,6 @@
 package com.server.board.dto;
 
+import com.server.board.entity.Board;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,14 @@ public class BoardPostDto {
     private String content; // 게시글 본문
 
     // 유저의 로그인 유효성 검증 변수 추가해야함!
+
+    public Board toBoard(){
+        Board board = new Board();
+        board.setTitle(title);
+        board.setContent(content);
+
+        return board;
+    }
 
 
 }
