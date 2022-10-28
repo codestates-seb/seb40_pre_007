bc4615b4f6c11b963b9729842704930273ae6eb9
JungSoyi <104415354+JungSoyi@users.noreply.github.com>
2022-10-28 22:20:33 +0900

    Update Board.java

--- server/src/main/java/com/server/domain/board/entity/Board.java
+++ server/src/main/java/com/server/domain/board/entity/Board.java
@@ -5,17 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 public class Board extends BaseTimeEntity {
 
     @Id // 데이터베이스 테이블의 BOARD 테이블과 매핑
