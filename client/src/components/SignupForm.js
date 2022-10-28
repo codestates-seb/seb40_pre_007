607c0c0c7a556786513f8de532c6a75c7eb3c96f
Hyejung Park <hyejj19@naver.com>
2022-10-28 22:21:44 +0900

    Feature/hyejung/signup#45 (#51)
    * 🐛 NavBar z-index 추가
    
    * 💄 컬러 추가
    
    * ✨ 소셜 로그인 버튼 text props 추가
    
    * 💄 input 사이즈, 라벨 텍스트 크기 수정
    
    * 💄 소셜 Sign up 버튼 텍스트 수정 및 간격 수정
    
    * 💄 회원가입 페이지 구현

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -5,16 +5,46 @@ export const SignupForm = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex py-2 justify-center items-center flex-col space-y-2">
-        <GoogleBtn />
-        <GithubBtn />
-        <FacebookBtn />
+        <GoogleBtn text={"Sign up"} />
+        <GithubBtn text={"Sign up"} />
+        <FacebookBtn text={"Sign up"} />
       </div>
       <div className="bg-white w-[300px] py-7 px-6 mt-2 rounded-md shadow-lg ">
-        <form className="flex flex-col space-y-5">
+        <form className="flex flex-col space-y-4">
           <Input label={"Display name"} id={"displayName"} />
           <Input label={"Email"} id={"email"} />
           <Input label={"Password"} id={"password"} />
+          <p className="text-gray-500 text-sm top-0">
+            Passwords must contain at least eight characters, including at least
+            1 letter and 1 number.
+          </p>
           <LargeBtn>Sign up</LargeBtn>
+          <p className="text-gray-500 text-sm top-0 [&>a]:text-main-blue">
+            By clicking “Sign up”, you agree to our
+            <a
+              href="https://stackoverflow.com/legal/terms-of-service/public"
+              className="hover:text-light-blue"
+            >
+              {" "}
+              terms of service
+            </a>
+            ,
+            <a
+              href="https://stackoverflow.com/legal/privacy-policy"
+              className="hover:text-light-blue"
+            >
+              {" "}
+              privacy policy{" "}
+            </a>
+            and
+            <a
+              href="https://stackoverflow.com/legal/cookie-policy"
+              className="hover:text-light-blue"
+            >
+              {" "}
+              cookie policy
+            </a>
+          </p>
         </form>
       </div>
     </div>
