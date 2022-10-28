27437762cc1d7d3565b92762e648bc8d9d36d8b3
Hyejung Park <hyejj19@naver.com>
2022-10-28 22:18:08 +0900

    💄 회원가입 페이지 구현

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,14 +1,108 @@
 import { NavBar } from "../../components/NavBar";
 import { SignupForm } from "../../components/SignupForm";
+import { Link } from "react-router-dom";
 
 export const Signup = () => {
   return (
-    <div className="h-screen w-screen bg-main-gray flex justify-center items-center flex-col">
+    <div className="h-screen w-screen bg-main-gray flex justify-center items-center">
       <NavBar />
-      <SignupForm />
-      <div className="flex space-x-2 mt-10 text-sm">
-        <span>{`Don't have an account?`}</span>
-        <span className="text-main-blue">Sign up</span>
+      {/* 좌측 리스트 */}
+      <div className="hidden md:flex mx-10 space-y-8 flex-col">
+        <span className="text-3xl ">Join the Stack Overflow community</span>
+        <ul className="space-y-8">
+          <li className="flex space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+              />
+            </svg>
+
+            <span>Get unstuck — ask a question</span>
+          </li>
+          <li className="flex space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+              />
+            </svg>
+
+            <li>Unlock new privileges like voting and commenting</li>
+          </li>
+          <li className="flex space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
+            </svg>
+
+            <li>Save your favorite tags, filters, and jobs</li>
+          </li>
+          <li className="flex space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0"
+              />
+            </svg>
+            <li>Earn reputation and badges</li>
+          </li>
+        </ul>
+        <div className="text-sm text-font-gray">
+          <p>Collaborate and share knowledge with a private group for FREE.</p>
+          <p className="text-main-blue">
+            Get Stack Overflow for Teams free for up to 50 users.
+          </p>
+        </div>
+      </div>
+      <div className="flex-col flex items-center justify-center">
+        <div className="w-[400px] mb-5 md:hidden">
+          <span className="text-2xl">
+            Create Your Stack Overflow account. It’s free and only takes a
+            minute.
+          </span>
+        </div>
+        <SignupForm />
+        <div className="flex space-x-2 mt-10 text-sm">
+          <span>{`Already have an account?`}</span>
+          <Link to={"/"}>
+            <span className="text-main-blue"> Log in</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
