d65dfcfc11e06551d7832479b8bc12515b542a2d
Hyejung Park <hyejj19@naver.com>
2022-10-28 22:16:17 +0900

    ✨ 소셜 로그인 버튼 text props 추가

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -1,6 +1,6 @@
 const ICON_CLASS_NAME = "w-6 h-6";
 
-export const GoogleBtn = () => {
+export const GoogleBtn = ({ text = "Log in" }) => {
   return (
     <button className="w-[300px] box-border bg-white border-button-border-gray border rounded-md py-2 flex justify-center items-center space-x-2 hover:bg-light-gray ">
       <div>
@@ -37,11 +37,11 @@ export const GoogleBtn = () => {
           />
         </svg>
       </div>
-      <span className="text-sm">Log in with Google</span>
+      <span className="text-sm">{text} with Google</span>
     </button>
   );
 };
-export const GithubBtn = () => {
+export const GithubBtn = ({ text = "Log in" }) => {
   return (
     <button className="w-[300px] box-border bg- bg-[#343a40] hover:bg-[#262729] border-button-border-gray border rounded-md py-2 flex justify-center items-center space-x-2 ">
       <div>
@@ -56,11 +56,11 @@ export const GithubBtn = () => {
           <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
         </svg>
       </div>
-      <span className="text-sm text-white">Log in with Github</span>
+      <span className="text-sm text-white">{text} with Github</span>
     </button>
   );
 };
-export const FacebookBtn = () => {
+export const FacebookBtn = ({ text = "Log in" }) => {
   return (
     <button className="w-[300px] box-border bg- bg-[#365899] hover:bg-[#29487D] border-button-border-gray border rounded-md py-2 flex justify-center items-center space-x-2 ">
       <div>
@@ -78,7 +78,7 @@ export const FacebookBtn = () => {
           />
         </svg>
       </div>
-      <span className="text-sm text-white">Log in with Github</span>
+      <span className="text-sm text-white">{text} with Github</span>
     </button>
   );
 };
