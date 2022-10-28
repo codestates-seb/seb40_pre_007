ae0bbcd7a7555f9974ac910ed669d4c127b2e404
Hyejung Park <hyejj19@naver.com>
2022-10-28 09:20:58 +0900

    💄 Login 페이지 작업중

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,10 +1,21 @@
 import { NavBar } from "../../components/NavBar";
+import {
+  GoogleBtn,
+  GithubBtn,
+  FacebookBtn,
+  LargeBtn,
+} from "../../components/Buttons";
 
 export const Login = () => {
   return (
-    <div className="w-full">
+    <div className="w-flex h-screen w-screen bg-main-gray">
       <NavBar />
-      {/* <h1>Login</h1> */}
+      <div className="flex w-screen h-screen justify-center items-center flex-col space-y-2">
+        <GoogleBtn />
+        <GithubBtn />
+        <FacebookBtn />
+        <LargeBtn>Log in</LargeBtn>
+      </div>
     </div>
   );
 };
