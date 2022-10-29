e53e0eb4a87ad3076b5981f21deef1fbcc7a5a4b
JungSoyi <soi0125@naver.com>
2022-10-30 02:12:00 +0900

    :recycle: refactor : global exception code 수정

--- server/src/main/java/com/server/global/exception/ExceptionCode.java
+++ server/src/main/java/com/server/global/exception/ExceptionCode.java
@@ -5,8 +5,14 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum ExceptionCode {
-    ACCOUNT_NOT_FOUND(404, "Account Not Found"),
-    BOARD_NOT_FOUND(400,"Board Not Found");
+    ACCOUNT_NOT_FOUND(404, "Account Not Found"), // 사용자를 찾을 수 없음
+    UNAUTHORIZED_ACCOUNT(401, "Unauthorized Account"), // 사용자가 유효한 인증 자격 증명이 없음
+    ACCESS_DENIED_ACCOUNT(403, "Access Denied Account"), // 서버에 요청 되었으나 사용자 권한이 없음
+    ACCOUNT_EXISTS(409, "Account Exists"), // 이미 존재하는 사용자
+    BOARD_NOT_FOUND(404,"Board Not Found"), // 게시글을 찾을 수 없음
+    BOARD_EXISTS(409, "Board Exists"), // 게시글이 이미 존재함
+    ANSWER_NOT_FOUND(404, "Answer Not Found"), // 답변을 찾을 수 없음
+    ANSWER_EXISTS(409, "Answer Exists"); // 이미 답변이 존재함
 
     @Getter
     private int status;
