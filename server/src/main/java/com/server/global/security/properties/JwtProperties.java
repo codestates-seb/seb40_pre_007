581722348a5e67db1c5827e767dbb4f47d1c8c68
JungSoyi <soi0125@naver.com>
2022-10-31 11:54:42 +0900

    :fire: rem : security 삭제

--- server/src/main/java/com/server/global/security/properties/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.server.global.security.properties;
-
-public interface JwtProperties {
-    String SECRET = "28d93268-a80b-4594-918b-5926a79a49ed";
-    int EXPIRATION_TIME = 1000 * 60 * 60;
-    String TOKEN_PREFIX = "Bearer";
-    String HEADER_STRING = "Authorization";
-}
