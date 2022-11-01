c22341cc157db0f62f65c9bd1559a96fd6ba4bac
Hyejung Park <hyejj19@naver.com>
2022-11-02 00:22:16 +0900

    ✨ axios client Authorization header 추가

--- client/src/client/client.js
+++ client/src/client/client.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const accessToken = localStorage.getItem("accessToken") || "";
+
 export const client = axios.create({
   baseURL: "http://localhost:4000/",
-  withCredentials: true,
+  headers: {
+    withCredentials: true,
+    Authorization: `Bearer ${accessToken}`,
+  },
 });
