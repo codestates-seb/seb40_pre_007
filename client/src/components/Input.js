1424ae2143e6bab50076e4876a6e534ac344a5a2
Hyejung Park <hyejj19@naver.com>
2022-11-02 08:30:07 +0900

    📝 props에 대한 주석 추가

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,3 +1,9 @@
+/*
+ * label : input에 표시할 label 이름입니다.
+ * register : react-hook-form 으로 등록해주신 register 객체
+ * errorMsg : 프론트단 유효성 검사 실패시 출력할 메시지
+ * failedMsg : 서버에서 요청 거부시 출력할 메시지
+ */
 export const Input = ({
   label,
   register = { name: "text" },
@@ -11,6 +17,7 @@ export const Input = ({
       <label htmlFor={register.name} className="font-extrabold text-md">
         {label}
       </label>
+
       <input
         id={register.name}
         type={register.name}
@@ -23,6 +30,7 @@ export const Input = ({
         border rounded-md focus:outline-none focus:ring-4 border-input-border-gray py-1.5 px-3 w-full`}
         {...register}
       />
+
       {errorMsg ? <span className="text-xs text-red-500">{error}</span> : null}
     </div>
   );
