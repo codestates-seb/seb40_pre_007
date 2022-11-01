99675ea2f410b976298091390b8a371c3ae610ea
Hyejung Park <hyejj19@naver.com>
2022-11-02 08:29:47 +0900

    ✨ proxy 관련 설정 추가

--- client/src/client/client.js
+++ client/src/client/client.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const accessToken = localStorage.getItem("accessToken") || "";
 
 export const client = axios.create({
-  baseURL: "http://localhost:4000/",
   headers: {
     withCredentials: true,
     Authorization: `Bearer ${accessToken}`,
+    "Content-Type": `application/json`,
   },
 });
