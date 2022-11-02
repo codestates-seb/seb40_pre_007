b0352fe3c26e891e15397c0a8f2440a72cbb5980
Hyejung Park <hyejj19@naver.com>
2022-11-02 18:56:29 +0900

    Feature/hyejung/detail answer#93 (#98)
    * ♻️ cors 권한 허용
    
    * ♻️ baseURL 수정
    
    * ✨ 답변 조회, 생성기능 구현
    
    * ✨ Main 페이지 게시글 전체조회 uri 수정

--- client/src/client/client.js
+++ client/src/client/client.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const accessToken = localStorage.getItem("accessToken") || "";
 
 export const client = axios.create({
+  baseURL: "http://ec2-3-34-19-99.ap-northeast-2.compute.amazonaws.com:8080",
   headers: {
     withCredentials: true,
     Authorization: `${accessToken}`,
