a76279f0cd3ed54a7b75f5d37d752d717a394cb2
hyeongiii <hyeongiii0811@gmail.com>
2022-11-02 15:14:06 +0900

    :art: styling : CORS 설정 수정

--- server/src/main/java/com/server/global/security/jwt/JwtProperties.java
+++ server/src/main/java/com/server/global/security/jwt/JwtProperties.java
@@ -3,7 +3,7 @@ package com.server.global.security.jwt;
 public interface JwtProperties {
 
     String SECRET = "28d93268-a80b-4594-918b-5926a79a49ed";
-    int EXPIRATION_TIME = 30;
+    int EXPIRATION_TIME = 60;
     String TOKEN_PREFIX = "Bearer";
     String HEADER_STRING = "Authorization";
 
