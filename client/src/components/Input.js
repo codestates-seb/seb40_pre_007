7fc5875c5f26baa560c258c0c985af5d389c3173
Hyejung Park <hyejj19@naver.com>
2022-11-02 09:21:13 +0900

    Feature/hyejung/login logic#63 (#91)
    * 📝 라이브러리 추가
    
    * 📝 더미데이터 추가
    
    * ✨ 리코일 적용
    
    * ✨ axios 클라이언트 추가
    
    * 🐛 Button onClick 수정
    
    * ✨ 로그인 로직 구현중
    
    * ✨ Form과 input에 react-hook-form 적용
    
    * ✨ 로그인 상태, accessToken 상태 추가
    
    * ✨ axios client Authorization header 추가
    
    * ✨ 로그인 유효성 검사 추가
    
    * ♻️ atoms accessToken 수정
    
    * ✨ proxy 관련 설정 추가
    
    * 📝 props에 대한 주석 추가
    
    * ♻️ NavBar 로그인,로그아웃 버튼 기능 수정
    
    * ✨ 로그인 기능 구현(추후 수정 필요)
    
    * 🔥 json 더미 데이터 삭제
    
    * ♻️  jwt 요청, 로그인 payload 수정

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,17 +1,38 @@
-export const Input = ({ label, register = "text" }) => {
-  console.log(register);
+
+/*
+ * label : input에 표시할 label 이름입니다.
+ * register : react-hook-form 으로 등록해주신 register 객체
+ * errorMsg : 프론트단 유효성 검사 실패시 출력할 메시지
+ * failedMsg : 서버에서 요청 거부시 출력할 메시지
+ */
+export const Input = ({
+  label,
+  register = { name: "text" },
+  errorMsg,
+  failedMsg,
+}) => {
+  const error = errorMsg[register.name]?.message || null;
   return (
     <div className="space-y-1 flex flex-col">
       <label htmlFor={register.name} className="font-extrabold text-md">
         {label}
       </label>
+
       <input
         id={register.name}
         type={register.name}
-        className="border  rounded-md focus:outline-none focus:border-main-blue focus:ring-4 focus:ring-blue-100 border-input-border-gray py-1.5 px-3 w-full"
-        required
+        className={`
+        ${
+          error || failedMsg
+            ? "focus:border-red-500 focus:ring-red-100"
+            : "focus:border-main-blue focus:ring-blue-100 "
+        }
+        border rounded-md focus:outline-none focus:ring-4 border-input-border-gray py-1.5 px-3 w-full`}
+
         {...register}
       />
+
+      {errorMsg ? <span className="text-xs text-red-500">{error}</span> : null}
     </div>
   );
 };
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,40 +1,47 @@
 import { FacebookBtn, GithubBtn, GoogleBtn, LargeBtn } from "./Buttons";
 import { Input } from "./Input";
 
-import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import { isLoginState } from "../recoil/atoms";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 import { client } from "../client/client";
+import { useState } from "react";
 
 export const LoginForm = () => {
-  // 로그인 상태 변경 함수
+  const naviagte = useNavigate();
   const setIsLogin = useSetRecoilState(isLoginState);
-  // 로그인 로직
-  useEffect(() => {
-    client
-      .post("/login", {
-        email: "coding@gmail.com",
-        password: "1111",
-      })
-      .then((res) => {
-        console.log(res.data);
-        if (res.data.accessToken)
-          // accessToken localStorage 저장
-          localStorage.setItem("accessToken", res.data.accessToken);
-        // 로그인 상태 변경
-        setIsLogin(true);
-      })
-      .catch((err) => console.error(err));
-  });
-
-  // 로그인 form 유효성 검사를 위한 함수
   const { register, handleSubmit } = useForm();
+  const [errorMsg, setErrorMsg] = useState("");
+  const [failedMsg, setFailedMsg] = useState("");
 
-  // 유효성 검사 통과시 적용할 로직
-  const onValid = () => {
-    return;
+  // 유효성 검사 통과시 로그인 로직
+  const onValid = ({ email: username, password }) => {
+    const payload = { username, password };
+
+    client
+      .post("/api/login", JSON.stringify(payload))
+      .then((res) => {
+        console.log(res.headers.get("Authorization"));
+        if (res.headers.get("Authorization"))
+          localStorage.setItem("accessToken", res.headers.get("Authorization"));
+        setIsLogin(true);
+        naviagte("/main");
+        setFailedMsg("");
+      })
+      .catch((err) => {
+        setFailedMsg(err.response.data);
+        setErrorMsg("");
+      });
+  };
+
+  console.log(failedMsg);
+
+  // 유효성 검사 실패시 input 클래스 변경 로직
+  const onInValid = (error) => {
+    setErrorMsg(error);
+    setFailedMsg("");
   };
 
   return (
@@ -44,14 +51,42 @@ export const LoginForm = () => {
         <GithubBtn />
         <FacebookBtn />
       </div>
-      <div className="bg-white w-[300px] py-7 px-6 mt-2 rounded-md shadow-lg ">
+
+      <div className="bg-white w-[300px] py-7 px-6 mt-2 rounded-md shadow-lg">
         <form
           className="flex flex-col space-y-5"
-          onSubmit={handleSubmit(onValid)}
+          onSubmit={handleSubmit(onValid, onInValid)}
         >
-          <Input label={"Email"} register={register("email")} />
-          <Input label={"Password"} register={register("password")} />
+          <Input
+            label={"Email"}
+            register={register("email", {
+              required: "이메일을 입력해주세요!",
+            })}
+            errorMsg={errorMsg}
+            failedMsg={failedMsg}
+          />
+
+          <Input
+            label={"Password"}
+            register={register("password", {
+              required: "비밀번호를 입력해주세요!",
+              minLength: {
+                value: 8,
+                message: "비밀번호는 8글자 이상입니다.",
+              },
+            })}
+            errorMsg={errorMsg}
+            failedMsg={failedMsg}
+          />
+
           <LargeBtn>Log in</LargeBtn>
+
+          {failedMsg ? (
+            <span className="text-xs text-red-500 m-auto">
+              {"로그인 실패시 서버 응답이 이곳에 출력됩니다. 현재 오류가 발생하여 추후 수정 예정입니다 (__)" ||
+                failedMsg}
+            </span>
+          ) : null}
         </form>
       </div>
     </div>
