5889085da44045adf0ea0abb41173df69954ca4f
hyeongiii <hyeongiii0811@gmail.com>
2022-11-03 16:17:35 +0900

    :white_check_mark: test : accountRepository test

---
+++ server/src/test/java/com/server/account/repository/AccountRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.server.account.repository;
+
+import com.server.domain.account.entity.Account;
+import com.server.domain.account.repository.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataJpaTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Test
+    void saveAccountTest() {
+        // given
+        Account account = new Account("hyeongiii@gmail.com", "이가형", "loveI1E5!");
+
+        // when
+        Account savedAccount = accountRepository.save(account);
+
+        // then
+        assertNotNull(savedAccount);
+        assertTrue(account.getDisplayName().equals(savedAccount.getDisplayName()));
+        assertTrue(account.getEmail().equals(savedAccount.getEmail()));
+        assertTrue(account.getPassword().equals(savedAccount.getPassword()));
+    }
+
+    @Test
+    void findByEmailTest() {
+        // given
+        Account account = new Account("hyeongiii@gmail.com", "이가형", "loveI1E5!");
+
+        // when
+        accountRepository.save(account);
+        Optional<Account> findAccount = accountRepository.findByEmail(account.getEmail());
+
+        // then
+        assertTrue(findAccount.isPresent());
+        assertTrue(account.getEmail().equals(findAccount.get().getEmail()));
+    }
+}
