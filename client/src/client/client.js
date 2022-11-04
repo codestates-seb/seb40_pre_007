d66ecb70cbfbaeb3a3ee3f0b2c257ed14fa0799b
Hyejung Park <hyejj19@naver.com>
2022-11-04 10:47:48 +0900

    ✨ feat: .env 파일 추가

--- client/src/client/client.js
+++ client/src/client/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const accessToken = localStorage.getItem("accessToken") || "";
 
 export const client = axios.create({
-  baseURL: "http://ec2-3-34-19-99.ap-northeast-2.compute.amazonaws.com:8080",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     withCredentials: true,
     Authorization: `${accessToken}`,
